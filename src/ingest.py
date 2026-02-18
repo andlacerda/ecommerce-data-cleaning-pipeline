499df6cdb0e0ac56ea8e442b128725611ba56a39
Anderson <andersonlacerdadev@gmail.com>
2026-02-17 23:46:15 -0300

    Finnished Injection.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,14 +1,47 @@
 import kagglehub as kh
 import os
+import sys
+import zipfile
 
-# Coloque seu token diretamente aqui
-TOKEN_NUMBER = "PLACE YOUR TOKEN NUMBER HERE..."
+def run_ingestion():
+    try :
+        # Set your Kaggle API token here
+        TOKEN_NUMBER = "PLACE YOUR TOKEN NUMBER HERE..."
 
-# Token Configuration
-os.environ["KAGGLE_API_TOKEN"] = TOKEN_NUMBER
+        # Token Configuration
+        os.environ["KAGGLE_API_TOKEN"] = TOKEN_NUMBER
 
-# Download the File
-path = kh.dataset_download("olistbr/brazilian-ecommerce",
-                            path="olist_orders_dataset.csv")
+        # Download the File
+        path = kh.dataset_download("olistbr/brazilian-ecommerce",
+                                path="olist_orders_dataset.csv")
+        destination = "../data/raw/olist_orders_dataset.zip"
 
-print("Path to dataset files:", path)
+        # Moving the File
+        os.makedirs("../data/raw",exist_ok=True)
+
+        # Remove existing file to avoid conflicts
+        if os.path.exists(destination):
+            os.remove(destination)
+
+        # Move downloaded file to the raw data folder
+        os.rename(path,destination)
+
+        # Extract the zip file and then delete the zip
+        with zipfile.ZipFile(destination, 'r') as zip_ref:
+            zip_ref.extractall("../data/raw/")
+            print(f"File Extracted Sucess.")
+        
+        if os.path.exists(destination):
+            os.remove(destination)
+            print("Zip file removed")
+
+        print(f"Ingestion completed successfully: {destination}")
+
+    except Exception as e:
+        # Error handling: execute if the ingestion fails
+        print(f"Error during ingestion process: {e}")
+        # Terminate execution with error code
+        sys.exit(1)
+
+if __name__ == "__main__":
+    run_ingestion()
